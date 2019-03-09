4a03048941acfa43d7dcbb4c09d75c7f1337a637
Justin Lim <nockmss@gmail.com>
2019-03-09 20:30:46 +0000

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Post from './components/Post';
+ import logo from './logo.svg';
+ import './App.css';
+ 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <Link to="/">
+              <img src={logo} className="App-logo" alt="logo" />
+            </Link>
+            <h1 className="App-title">Welcome to React</h1>
+            <Link to="/">Home</Link>
+            <Link to="/post/">Post</Link>
+          </header>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/post/" component={Post} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
 
-export default App;
+ export default App;
+ 
