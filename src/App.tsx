d44693398bf6df8f3a409a6560f85c8482a8ab8b
Justin Lim <nockmss@gmail.com>
2019-03-10 19:30:39 +0000

    tslint fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import './App.css';
 import Home from './components/Home';
 import Post from './components/Post';
- import logo from './logo.svg';
- import './App.css';
- 
+import logo from './logo.svg';
+
 class App extends Component {
-  render() {
+  public render() {
     return (
       <BrowserRouter>
-        <div className="App">
-          <header className="App-header">
-            <Link to="/">
-              <img src={logo} className="App-logo" alt="logo" />
+        <div className='App'>
+          <header className='App-header'>
+            <Link to='/'>
+              <img src={logo} className='App-logo' alt='logo' />
             </Link>
-            <h1 className="App-title">Auto build! With typescript.</h1>
-            <Link to="/">Home</Link>
-            <Link to="/post/">Post</Link>
+            <h1 className='App-title'>Auto build! With typescript.</h1>
+            <Link to='/'>Home</Link>
+            <Link to='/post/'>Post</Link>
           </header>
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/post/" component={Post} />
+            <Route exact path='/' component={Home} />
+            <Route path='/post/' component={Post} />
           </Switch>
         </div>
       </BrowserRouter>
@@ -28,5 +28,4 @@ class App extends Component {
   }
 }
 
- export default App;
- 
+export default App;
