5dd0f654dc4a4d912bdb1169ebf72805f7708bf1
Justin Lim <nockmss@gmail.com>
2019-03-11 00:12:36 +0000

    Full background colour

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import Main from './components/Main';
+import style from './style/index';
 
 class App extends Component {
 	public render() {
 		return (
-			<Main />
+			<div
+				style={{
+					height: '100vh',
+					minHeight: '100vh',
+					backgroundColor: style.background,
+				}}
+			>
+				<Main />
+			</div>
 		);
 	}
 }
